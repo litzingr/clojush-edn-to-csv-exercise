995ff44307bac98f09fe5273a35e6b1efc4bcaf3
Zach Litzinger <litzi005@morris.umn.edu>
2018-03-09 14:52:18 -0600

    setting up for all the files
    setting up for all the files
    
    setting up for all the files
    
    setting up for all the files

--- src/edn2csv/core.clj
+++ src/edn2csv/core.clj
@@ -12,6 +12,11 @@
 
 ; The header line for the Individuals CSV file
 (def individuals-header-line "UUID:ID(Individual),Generation:int,Location:int,:LABEL")
+(def semantics-header-line "UUID:ID(Semantics),TotalError:int,:LABEL")
+(def errors-header-line "UUID:ID(Error),ErrorValue:int,Location:int,:LABEL")
+(def parent-of-header-line ":START_ID(Individual),GeneticOperator,:END_ID(Individual),:TYPE")
+(def individual-semantics-header-line ":START_ID(Individual),:END_ID(Semantics),:TYPE")
+(def semantics-error-header-line "START_ID(Semantics),:END_ID(Error),:TYPE")
 
 ; Ignores (i.e., returns nil) any EDN entries that don't have the
 ; 'clojure/individual tag.
@@ -41,7 +46,12 @@
   1)
 
 (defn edn->csv-sequential [edn-file individuals-csv-file semantics-csv-file errors-csv-file parent-of-csv-file individual-semantics-csv-file semantics-error-csv-file]
-  (with-open [individuals-out-file (io/writer individuals-csv-file)]
+  (with-open [individuals-out-file (io/writer individuals-csv-file)
+              semantics-out-file (io/writer semantics-csv-file)
+              errors-out-file (io/writer errors-csv-file)
+              parent-of-out-file (io/writer parent-of-csv-file)
+              individual-semantics-out-file (io/writer individual-semantics-csv-file)
+              semantics-error-out-file (io/writer semantics-error-csv-file)]
     (safe-println individuals-out-file individuals-header-line)
     (->>
       (line-seq (io/reader edn-file))
@@ -53,7 +63,12 @@
       )))
 
 (defn edn->csv-pmap [edn-file individuals-csv-file semantics-csv-file errors-csv-file parent-of-csv-file individual-semantics-csv-file semantics-error-csv-file]
-  (with-open [individuals-out-file (io/writer individuals-csv-file)]
+  (with-open [individuals-out-file (io/writer individuals-csv-file)
+              semantics-out-file (io/writer semantics-csv-file)
+              errors-out-file (io/writer errors-csv-file)
+              parent-of-out-file (io/writer parent-of-csv-file)
+              individual-semantics-out-file (io/writer individual-semantics-csv-file)
+              semantics-error-out-file (io/writer semantics-error-csv-file)]
     (safe-println individuals-out-file individuals-header-line)
     (->>
       (line-seq (io/reader edn-file))
@@ -66,8 +81,18 @@
       )))
 
 (defn edn->csv-reducers [edn-file individuals-csv-file semantics-csv-file errors-csv-file parent-of-csv-file individual-semantics-csv-file semantics-error-csv-file]
-  (with-open [individuals-out-file (io/writer individuals-csv-file)]
+  (with-open [individuals-out-file (io/writer individuals-csv-file)
+              semantics-out-file (io/writer semantics-csv-file)
+              errors-out-file (io/writer errors-csv-file)
+              parent-of-out-file (io/writer parent-of-csv-file)
+              individual-semantics-out-file (io/writer individual-semantics-csv-file)
+              semantics-error-out-file (io/writer semantics-error-csv-file)]
     (safe-println individuals-out-file individuals-header-line)
+    (safe-println semantics-out-file semantics-header-line)
+    (safe-println errors-out-file errors-header-line)
+    (safe-println parent-of-out-file parent-of-header-line)
+    (safe-println individual-semantics-out-file individual-semantics-header-line)
+    (safe-println semantics-error-out-file semantics-error-header-line)
     (->>
       (iota/seq edn-file)
       (r/map (partial edn/read-string {:default individual-reader}))
